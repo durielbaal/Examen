e671a7b1f26a18488e78484a9fab81468363643d
durielbaal <michaelogwu1997@gmail.com>
2017-03-21 10:31:57 +0100

    Actualizaciones en nuestro proyecto

--- src/zumo/Main.java
+++ src/zumo/Main.java
@@ -12,29 +12,29 @@ package zumo;
 public class Main {
     public static void main(String[] args) {
 
-        acciones_Zumo();
+        acciones_Zumo(0.0);
     }
 
-    public static void acciones_Zumo() {
-        Zumo unDeposito;
+    public static void acciones_Zumo(double peso) {
+        Zumo depositoPomelo;
         int relleno;
         /*Creación de un depósito con zumo de pomelo, inicialmente contiene 20 litros, su capacidad máxima es de 40 litros
         el precio del litro es 2€*/
-        unDeposito= new Zumo(20,2,"Pomelo",40);
+        depositoPomelo= new Zumo(20,2,"Pomelo",40);
         
         try {
             System.out.println("Vamos a tomar zumo");
-            unDeposito.sacarZumo(5, 20);//Se intentan comprar 5 litros de zumo con 20€
+            depositoPomelo.sacarZumo(5, 20);//Se intentan comprar 5 litros de zumo con 20€
         } catch (Exception e) {
             System.out.println("Error al sacar zumo");
         }
         try {
             System.out.println("Rellenando depósito");
-            unDeposito.rellenar(30);//Se intenta rellenar el deposito añadiendo 30 litros
+            depositoPomelo.rellenar(30);//Se intenta rellenar el deposito añadiendo 30 litros
         } catch (Exception e) {
             System.out.println("Fallo al rellenar el depósito");
         }
-        relleno = unDeposito.obtenerLitros();
+        relleno = depositoPomelo.obtenerLitros();
         System.out.println("El depósito contiene " + relleno + " litros");
     }
 }
